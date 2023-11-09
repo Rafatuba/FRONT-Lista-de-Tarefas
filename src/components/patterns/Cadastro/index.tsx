33c9ce9a979583f353b243bb00306c95e54f290d
Rafatuba <rafaolly1@gmail.com>
2023-11-09 15:34:38 -0400

    Validações de cadastro com Yup

--- src/components/patterns/Cadastro/index.tsx
+++ src/components/patterns/Cadastro/index.tsx
@@ -1,61 +1,120 @@
 import React, { useState } from "react";
-import { TextInput, View, Image, TouchableOpacity, Text } from "react-native";
-import {styles} from './styles'
-import { theme } from '../../../theme'
-import logoImage from '../../../assets/logo2.png'
+import {
+  Alert,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import { styles } from "./styles";
+import { theme } from "../../../theme";
+import logoImage from "../../../assets/logo2.png";
+import * as Yup from "yup";
 
 import { cadastrar } from "../../../servicos/requsicoesFirebase";
 
 export function Cadastro() {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [confirmaSenha, setConfirmaSenha] = useState('');
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setConfirmaSenha] = useState("");
+  const [errors, setErrors] = useState<{
+    email: string;
+    senha: string;
+    confirmaSenha: string;
+  }>({
+    email: "",
+    senha: "",
+    confirmaSenha: "",
+  });
 
- async function realizarCadastro() {
-  await cadastrar(email, senha, confirmaSenha)
-  setEmail('')
-  setSenha('')
-  setConfirmaSenha('')
- }
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Digite um e-mail válido")
+      .required("O e-mail é obrigatório"),
+    senha: Yup.string()
+      .min(6, "A senha deve ter pelo menos 6 caracteres")
+      .required("A senha é obrigatória"),
+    confirmaSenha: Yup.string().oneOf(
+      [Yup.ref("senha"), null],
+      "As senhas não coincidem"
+    ),
+  });
+
+  async function realizarCadastro() {
+    try {
+      await validationSchema.validate(
+        { email, senha, confirmaSenha },
+        { abortEarly: false }
+      );
+      const resultado = await cadastrar(email, senha);
+      setEmail("");
+      setSenha("");
+      setConfirmaSenha("");
+      setErrors({ email: "", senha: "", confirmaSenha: "" });
+
+      if (resultado == "sucesso") {
+        Alert.alert("PARABÉNS!!! Você foi cadastrado com sucesso!");
+        setEmail("");
+        setSenha("");
+        setConfirmaSenha("");
+      } else {
+        Alert.alert(resultado);
+      }
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const newErrors: {
+          email: string;
+          senha: string;
+          confirmaSenha: string;
+        } = { email: "", senha: "", confirmaSenha: "" };
+        error.inner.forEach((err) => {
+          newErrors[err.path] = err.message;
+        });
+        setErrors(newErrors);
+      } else {
+        console.error(error);
+      }
+    }
+  }
 
   return (
     <View style={styles.headerContainer}>
-      <Image source={logoImage} style={styles.img}/>
-      <TextInput 
-          style={[
-            styles.input,
-          ]} 
-          value={email}
-          onChangeText={texto => setEmail(texto)}
-          placeholder='Novo e-mail'
-          placeholderTextColor={theme.colors.base.gray300}
-          />
-      <TextInput 
-          style={[
-            styles.input,
-          ]} 
-          value={senha}
-          onChangeText={texto => setSenha(texto)}
-          placeholder='Nova senha'
-          placeholderTextColor={theme.colors.base.gray300}
-          secureTextEntry
-          />
-      <TextInput 
-        style={[
-          styles.input,
-        ]} 
-        value={confirmaSenha}
-        onChangeText={texto => setConfirmaSenha(texto)}
-        placeholder='Confirmar nova senha'
+      <Image source={logoImage} style={styles.img} />
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={(texto) => setEmail(texto)}
+        placeholder="Novo e-mail"
+        placeholderTextColor={theme.colors.base.gray300}
+      />
+      {errors.email && <Text style={styles.errorText}>{errors.email}</Text>}
+      <TextInput
+        style={styles.input}
+        value={senha}
+        onChangeText={(texto) => setSenha(texto)}
+        placeholder="Nova senha"
         placeholderTextColor={theme.colors.base.gray300}
         secureTextEntry
       />
-      <TouchableOpacity 
+      {errors.senha && <Text style={styles.errorText}>{errors.senha}</Text>}
+      <TextInput
+        style={styles.input}
+        value={confirmaSenha}
+        onChangeText={(texto) => setConfirmaSenha(texto)}
+        placeholder="Confirmar nova senha"
+        placeholderTextColor={theme.colors.base.gray300}
+        secureTextEntry
+      />
+      {errors.confirmaSenha && (
+        <Text style={styles.errorText}>{errors.confirmaSenha}</Text>
+      )}
+      <TouchableOpacity
         style={styles.button}
         onPress={() => realizarCadastro()}
       >
-        <Text style={[styles.text]}>Cadastrar</Text>
+        <Text style={styles.text}>Cadastrar</Text>
       </TouchableOpacity>
     </View>
-  )
+  );
 }
--- src/servicos/requsicoesFirebase.jsx
+++ src/servicos/requsicoesFirebase.jsx
@@ -1,12 +1,33 @@
 import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
 
-export async function cadastrar(email, senha, confirmaSenha) {
-  createUserWithEmailAndPassword(auth, email, senha, confirmaSenha )
+function errosFirebase(error) {
+  let mensagem = '';
+  switch (error.code) {
+    case AuthErrorCodes.EMAIL_EXISTS:
+      mensagem = "E-mail já cadastrado.";
+      break;
+    case AuthErrorCodes.INVALID_EMAIL:
+      mensagem = "E-mail inválido";
+      break;
+    case AuthErrorCodes.WEAK_PASSWORD:
+      mensagem = "A senha deve possuir no mínimo 6 caracteres";
+      break;
+    default:
+      mensagem = "Erro desconhecido. Tente novamente mais tarde!"
+  }
+  return mensagem;
+}
+
+export async function cadastrar(email, senha) {
+  const resultado = await createUserWithEmailAndPassword(auth, email, senha )
   .then((dadosDoUsuario) => {
     console.log(dadosDoUsuario)
+    return 'sucesso'
   })
   .catch((error) => {
    console.log(error)
+   return errosFirebase(error)
   });
+  return resultado
 }
